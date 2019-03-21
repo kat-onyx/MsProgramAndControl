39c6fd4956b34f1b25f9076184361b6b5177880b
kat <sarah.kathryn.peters@gmail.com>
2019-03-21 16:25:13 -0700

    Fix for live

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     context: __dirname,
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, "docs"),
+        path: path.resolve(__dirname, "dist"),
         filename: 'main.js'
     },
     resolve: {
